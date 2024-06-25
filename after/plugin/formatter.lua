beef06ca5739b9fec70028a6583007961d3e2ca1
Ian Shakespeare <irshakes11@gmail.com>
2024-06-25 16:48:57 +0900

    formatter changes

--- after/plugin/formatter.lua
+++ after/plugin/formatter.lua
@@ -1,20 +1,13 @@
-local function format_python()
-  return {
-    exe = "python3 -m autopep8",
-    args = { "--in-place", "--aggressive" },
-    stdin = false,
-    ignore_exitcode = true,
-  }
-end
-
 require("formatter").setup {
   filetype = {
     javascript = require("formatter.filetypes.javascript").prettier,
     typescript = require("formatter.filetypes.typescript").prettier,
     javascriptreact = require("formatter.filetypes.javascriptreact").prettier,
     typescriptreact = require("formatter.filetypes.typescriptreact").prettier,
-    python = { format_python }
+    python = require("formatter.filetypes.python").autopep8,
+    go = require("formatter.filetypes.go").gofmt,
+    rust = require("formatter.filetypes.rust").rustfmt,
   }
 }
 
-vim.keymap.set("n", "<leader>cf", ":Format <cr>")
+vim.keymap.set("n", "<leader>cf", ":FormatWrite <cr>")
