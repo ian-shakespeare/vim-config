adbf24f91c0f7d652e34f8bb333ead7fb6c315e3
Ian Shakespeare <irshakes11@gmail.com>
2024-07-31 18:27:24 +0900

    added formatters for svelte and html

--- after/plugin/formatter.lua
+++ after/plugin/formatter.lua
@@ -4,9 +4,11 @@ require("formatter").setup {
     typescript = require("formatter.filetypes.typescript").prettier,
     javascriptreact = require("formatter.filetypes.javascriptreact").prettier,
     typescriptreact = require("formatter.filetypes.typescriptreact").prettier,
+    html = require("formatter.filetypes.html").prettier,
     python = require("formatter.filetypes.python").autopep8,
     go = require("formatter.filetypes.go").gofmt,
     rust = require("formatter.filetypes.rust").rustfmt,
+    svelte = require("formatter.filetypes.svelte").prettier,
   }
 }
 
