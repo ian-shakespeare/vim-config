990e1f9e663deafe16537e3a4bfdba408db338e3
Ian Shakespeare <irshakes11@gmail.com>
2024-10-17 11:51:08 -0600

    format on save

--- after/plugin/formatter.lua
+++ after/plugin/formatter.lua
@@ -14,3 +14,12 @@ require("formatter").setup {
 }
 
 vim.keymap.set("n", "<leader>cf", ":FormatWrite <cr>")
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+augroup("__formatter__", { clear = true })
+autocmd("BufWritePost", {
+	group = "__formatter__",
+	command = ":FormatWrite",
+})
