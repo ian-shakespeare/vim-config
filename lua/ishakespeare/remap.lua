61677d66696738cc2c0f51506ea17284d52242e7
Ian Shakespeare <ian@shakespeare.dev>
2025-01-29 09:48:36 -0700

    go err macro

--- lua/ishakespeare/remap.lua
+++ lua/ishakespeare/remap.lua
@@ -1,5 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>x", ":bd <bar> :bprev <cr>")
+vim.keymap.set("n", "<leader>x", ":bd <bar> :bprev <cr>", { silent = true })
 vim.keymap.set("n", "<tab>", vim.cmd.bnext)
 vim.keymap.set("n", "<S-tab>", vim.cmd.bprev)
+
+-- Go error handling
+vim.keymap.set("n", "@e", "oif err != nil {<Enter><Enter>}<Esc>ki<Tab>", { silent = true })
