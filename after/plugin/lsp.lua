b8687c250a0f64cdcf8a2a6cae3273f8c4893d1a
Ian Shakespeare <ian@shakespeare.dev>
2025-05-22 09:06:05 -0600

    fix typescript lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -43,7 +43,7 @@ lsp_config["dartls"].setup({
 require("mason").setup({})
 require("mason-lspconfig").setup({
   ensure_installed={
-    "tsserver",
+    "typescript-language-server",
     "pyright",
     "rust_analyzer",
     "gopls",
